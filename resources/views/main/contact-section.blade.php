5bfc25b47925b13e2b5fec5a38cdb7adc1f68c23
Dohd <alloicebrian@gmail.com>
2022-08-23 22:52:36 +0300

    Add contact statement

--- resources/views/main/contact-section.blade.php
+++ resources/views/main/contact-section.blade.php
@@ -3,7 +3,11 @@
 
     <div class="section-title">
       <h2>Contact</h2>
-      <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+      <p>
+        <b>DROP US A LINE.</b> <br>
+        Open for more communication with you. <br>
+        Use this form for any requests and suggestions.
+      </p>
     </div>
 
     <div class="row">
