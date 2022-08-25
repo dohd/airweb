0efbf56b09f931aacc00456958fa1dd8a592baf8
Dohd <alloicebrian@gmail.com>
2022-08-25 23:06:36 +0300

    Update services

--- resources/views/main/service-section.blade.php
+++ resources/views/main/service-section.blade.php
@@ -3,39 +3,46 @@
 
       <div class="section-title">
         <h2>Services</h2>
-        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        <p>
+          We aim to establish mutual benefit between us and our clients. This ensures
+          the highest standards are maintained. Call us for expert services.
+        </p>
       </div>
 
       <div class="row">
         <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
           <div class="icon-box">
             <div class="icon"><i class="bx bxl-dribbble"></i></div>
-            <h4><a href="">Lorem Ipsum</a></h4>
-            <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+            <h4><a href="">Supply and Installation of Chillers</a></h4>
+            <p>We Supply, install & commission different chillers of all makes and sizes. Fell free
+              to contact us for a free assessment of your requirements</p>
           </div>
         </div>
 
         <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
           <div class="icon-box">
             <div class="icon"><i class="bx bx-file"></i></div>
-            <h4><a href="">Sed ut perspici</a></h4>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+            <h4><a href="">VRF Air Conditioning</a></h4>
+            <p>Can support a height of up to 40M equivalent to 11 story building. The new
+              SMMS-i offers efficient savings with 3 highly efficient DC twin Rotary compressors.</p>
           </div>
         </div>
 
         <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
           <div class="icon-box">
             <div class="icon"><i class="bx bx-tachometer"></i></div>
-            <h4><a href="">Magni Dolores</a></h4>
-            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+            <h4><a href="">Toshiba and Carrier Split Systems</a></h4>
+            <p>Deliver the ultimate temperature control. One Compressor can serve up
+              to four interior units for exceptional flexibility and reliability.</p>
           </div>
         </div>
 
         <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
           <div class="icon-box">
             <div class="icon"><i class="bx bx-layer"></i></div>
-            <h4><a href="">Nemo Enim</a></h4>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+            <h4><a href="">Liberty High Density Data Center Cooling</a></h4>
+            <p> Available with open or closed architecture, and
+              pumped refrigerant-based or water-based technology.</p>
           </div>
         </div>
 
