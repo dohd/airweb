0d27daa76ba024ae0e34ca1bdb719c7a416b7915
Dohd <alloicebrian@gmail.com>
2022-08-25 23:06:15 +0300

    Update about us

--- resources/views/main/about-us-section.blade.php
+++ resources/views/main/about-us-section.blade.php
@@ -8,8 +8,11 @@
       <div class="row content">
         <div class="col-lg-6">
           <p>
-            We are particularly a service based company offering services such as 
-            air conditioning and mechanical ventilation system services. Our service include and not limited to;
+            Established in 2005 by our visionary and enterprising founder Eng. Francis K. Mwatha. Airweb (EA) Ltd
+            deals with (HVAC-R) Heating, Ventilation, Electrical, Air Conditioning & Refrigeration
+            services with the aim offering best services to customers. We are one of the leading providers of HVAC-R services
+            in East Africa and we take pride in the solid relationship betweeen us and customers having built a
+            reputation over the years.
           </p>
           <ul>
             <li><i class="ri-check-double-line"></i> Repair, maintenance and installation of refrigeartion equipments</li>
@@ -19,9 +22,9 @@
         </div>
         <div class="col-lg-6 pt-4 pt-lg-0">
           <p>
-            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            In today's fast paced and dynamically evolving world it becomes essential that every business evolves
+            and adopts to the changing times. At Airweb (EA) Ltd we do just that. Our adaptation policy ensures we meet customer
+            requirements at all costs.
           </p>
           <a href="#" class="btn-learn-more">Learn More</a>
         </div>
