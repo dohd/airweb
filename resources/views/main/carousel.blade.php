a81f7710721a97bbeda60cfa810aced2407472fa
dohd <alloicebrian@gmail.com>
2022-10-23 23:08:56 +0300

    Update carousel block

--- resources/views/main/carousel.blade.php
+++ resources/views/main/carousel.blade.php
@@ -1,36 +1,36 @@
-{{-- <section>
-    <h5 class="">carousel</h5>
-    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-        <!-- Indicators -->
-        
-        <ol class="carousel-indicators">
-            <li data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></li>
-            <li data-bs-target="#myCarousel" data-bs-slide-to="1"></li>
-            <li data-bs-target="#myCarousel" data-bs-slide-to="2"></li>
-        </ol>
-           
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="d-block w-100" src="{{ asset('img/durability.jpeg') }}" alt="Durability">
-            </div>
-    
-            <div class="carousle-item">
-                <img class="d-block w-100" src="{{ asset('img/hero.png') }}" alt="Hero">
-            </div>
-    
-            <div class="carousel-item">
-                <img class="d-block w-100" src="{{ asset('img/cta-bg.jpg') }}" alt="cta">
-            </div>
+
+<div id="mycarousel" class="carousel slide" data-bs-ride="carousel">
+    {{-- Indicators --}}
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#mycarousel" data-bs-slide-to="0" class="active"></button>
+        <button type="button" data-bs-target="#mycarousel" data-bs-slide-to="1"></button>
+        <button type="button" data-bs-target="#mycarousel" data-bs-slide-to="2"></button>
+        <button type="button" data-bs-target="#mycarousel" data-bs-slide-to="3"></button>
+    </div>
+
+    {{-- Wrapper for Slides --}}
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="{{ asset('img/duct-work.jpeg') }}" alt="" class="d-block w-100" height="350">
         </div>
-        <!-- Left and right controls -->
-        <a class="carousel-control-prev" href="#myCarousel" role="button" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#myCarousel" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>    
-</section> --}}
+        <div class="carousel-item">
+            <img src="{{ asset('img/electrical-work.jpg') }}" alt="" class="d-block w-100" height="350">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('img/plumbing-work.jpeg') }}" alt="" class="d-block w-100" height="350">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('img/raised-floor.jpeg') }}" alt="" class="d-block w-100" height="350">
+        </div>
+    </div>
+
+    {{-- Left and Right Controls --}}
+    <a href="#mycarousel" class="carousel-control-prev" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </a>
+    <a href="#mycarousel" class="carousel-control-next" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </a>
+</div>
