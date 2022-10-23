a03108423449de9fc5634de4ab4de0c6510e31ea
dohd <alloicebrian@gmail.com>
2022-10-23 10:06:11 +0300

    Update about-us

--- resources/views/main/about-us-section.blade.php
+++ resources/views/main/about-us-section.blade.php
@@ -15,12 +15,22 @@
             reputation over the years.
           </p>
           <ul>
-            <li><i class="ri-check-double-line"></i> Repair, maintenance and installation of refrigeartion equipments</li>
+            <li><i class="ri-check-double-line"></i> Repair, maintenance and installation of refregiration equipments</li>
             <li><i class="ri-check-double-line"></i> Installation of air conditioning units</li>
             <li><i class="ri-check-double-line"></i> Installation of mechanical ventilation systems</li>
+            <li><i class="ri-check-double-line"></i> Duct, electrical and plumbing works</li>
+            <li><i class="ri-check-double-line"></i> Guarantees given are subject to annual services as prescribed by AIRWEB (EA) LTD</li>
           </ul>
         </div>
         <div class="col-lg-6 pt-4 pt-lg-0">
+          <p>
+            As the providers of air conditioning and ventilation services we understand that our customers are our
+            biggest asset and we aim to keep them happy and pleased at all times. This is why we have become a
+            well-known and recognized service in our area and have been recommended as one of the best places
+            to get professional HVAC-R services. For this we are ever so grateful to our client base, and our way of
+            thanking them is by constantly improvising on our products and services and our customer care
+            operations.
+          </p>
           <p>
             In today's fast paced and dynamically evolving world it becomes essential that every business evolves
             and adopts to the changing times. At Airweb (EA) Ltd we do just that. Our adaptation policy ensures we meet customer
