6a75328bc3a6c4f3cf89bf2a4c7fe88cecaf6e5d
dohd <alloicebrian@gmail.com>
2022-10-23 23:09:11 +0300

    Update cta format

--- resources/views/main/cta-section.blade.php
+++ resources/views/main/cta-section.blade.php
@@ -1,17 +1,16 @@
 <section id="cta" class="cta">
     <div class="container" data-aos="zoom-in">
-
-      <div class="row">
-        <div class="col-lg-9 text-center text-lg-start">
-          <h3>Call To Action</h3>
-          <p>
-            What are you waiting for ? Call us today, we are at your service. Feel comfortable using our Air-conditioning systems and worry less about your data centers. We have got you covered. 
-          </p>
+        <div class="row">
+            <div class="col-lg-9 text-center text-lg-start">
+                <h3>Call To Action</h3>
+                <p>
+                    What are you waiting for ? Call us today, we are at your service. Feel comfortable using our
+                    Air-conditioning systems and worry less about your data centers. We have got you covered.
+                </p>
+            </div>
+            <div class="col-lg-3 cta-btn-container text-center">
+                <a class="cta-btn align-middle" href="#"><b>+254 722-144-088</b></a>
+            </div>
         </div>
-        <div class="col-lg-3 cta-btn-container text-center">
-          <a class="cta-btn align-middle" href="#"><b>+254 722-144-088</b></a>
-        </div>
-      </div>
-
     </div>
-  </section>
+</section>
