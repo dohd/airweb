0120ee0877915a74734751eb44a19d071894929a
dohd <alloicebrian@gmail.com>
2022-10-23 10:06:20 +0300

    Update services

--- resources/views/main/service-section.blade.php
+++ resources/views/main/service-section.blade.php
@@ -45,8 +45,55 @@
               pumped refrigerant-based or water-based technology.</p>
           </div>
         </div>
-
       </div>
 
+      <div class="row mt-3">
+        <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+          <div class="icon-box">
+            <div class="icon"><i class="bx bxl-dribbble"></i></div>
+            <h4><a href="">Industrial Cooling Tower</a></h4>
+            <p>Designed for the physical need of Industrial sites with rugged and servicable components.
+              Our Designs provides addional durability and higher capacity for applications
+              where cooling and air delivery needs are greater than what conventional air conditioning systems can handle. </p>
+          </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+          <div class="icon-box">
+            <div class="icon"><i class="bx bx-file"></i></div>
+            <h4><a href="">Refrigeration</a></h4>
+            <p>We supply and install different refrigeration systems ranging from Cold/Freezer Rooms and
+              Wine Cellars for residential and chillers with cooling capacities ranging from 703 kW to 1055 kW for commercial use.
+              We also supply and install Cold rooms for Food storage, Flower cold rooms and Medical & Mortuary Freezers.
+            </p>
+          </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
+          <div class="icon-box">
+            <div class="icon"><i class="bx bx-tachometer"></i></div>
+            <h4><a href="">Ducting</a></h4>
+            <p>
+              We are involved in the design, installation & maintenance of DURKEESOX air dispersion
+              system as well as steel & other forms of supply & extraction canopies for domes and commercial
+              applications. All our systems are designed and fixed using highest quality stainless steel.
+            </p>
+          </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+          <div class="icon-box">
+            <div class="icon"><i class="bx bx-layer"></i></div>
+            <h4><a href="">Maintenance</a></h4>
+            <p> 
+              We have a service department specifically dedicated for after-sales service. All air conditioners,
+              refrigeration, mechanical ventilation and electrical engineering should be serviced on an annual basis, as
+              prescribed by suppliers to; prolong the life-span of your air-conditioning systems, promote efficient operation.
+              <br>
+              Services are priced per unit, based on material and expertise costs.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
