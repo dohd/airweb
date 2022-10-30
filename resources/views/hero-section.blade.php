8d4c001724a50eb657d30c62364d31b453d18b23
dohd <alloicebrian@gmail.com>
2022-10-30 23:54:29 +0300

    Update call-us text

--- resources/views/hero-section.blade.php
+++ resources/views/hero-section.blade.php
@@ -2,36 +2,19 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1"
-                data-aos="fade-up" data-aos-delay="200">
+                data-aos="fade-up" data-aos-delay="100">
                 <h1>Airweb Refrigeration</h1>
                 <h1>Best for your business</h1>
                 <h2>
                     We specialize in Air Conditioning, Refrigeration, Mechanical Ventilation, Electrical, Plumbing,
                     & General Construction.
                 </h2>
-
                 <div class="call-us">
-                    <h2 class="text-white m-0">Call Us Now!</h2>
+                    <h2 class="text-white m-0 mb-1">Call Us Now!</h2>
                     <h2 class="text-white text-center m-0 border rounded p-1 w-50">+254 722-144-088</h2>
                 </div>
-{{-- 
-                <div class="row">
-                    <div class="col-6">
-                        <h2 class="text-white mt-1">Call Us Now!</h2>
-                    </div>
-                    <div class="col-6">
-                        <h2 class="text-white d-inline-block border rounded p-1">+254 722-144-088</h2>
-                    </div>
-                </div> --}}
-                
-                {{-- <div class="d-flex justify-content-between justify-content-lg-start">
-                    <a href="#about" class="btn-get-started scrollto">Get Started</a>
-                </div> --}}
             </div>
-            {{-- <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-                <img src="{{ asset('img/hero.png') }}" class="img-fluid animated" alt="">
-            </div> --}}
-            <div class="col-lg-6 order-1 order-lg-2 hero-slider">
+            <div class="col-lg-6 order-1 order-lg-2 hero-slider" data-aos="zoom-in" data-aos-delay="100">
                 @include('main.carousel')
             </div>
         </div>
